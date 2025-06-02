9349540922f851c1e41edac143323b84284f24d4
Yash Tibrewal <yashkush.tibrewal@gmail.com>
2025-06-02 17:10:53 +0530

    updated server running message

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ connectDb().then(()=>{
 
     // Start the server
     app.listen(port, () => {
-        console.log(`Server is running at http://localhost:${port}`);
+        console.log(`Server is running on port ${port}`);
     });
 
 });
